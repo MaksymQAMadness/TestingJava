017dccbc49d53ffa176806acafe8446befb5ae3e
maktre <maktre@qamadness.com>
2025-10-16 02:58:20 +0300

    HW11 commit 2

--- post.test.js
+++ post.test.js
@@ -1,6 +1,6 @@
 const axios = require('axios')
 
-describe('HW11 task1', () => {
+describe('HW11', () => {
     test('Task1', async () => {
         const response = await axios.get('https://jsonplaceholder.typicode.com/posts/1')
         const post = response.data
@@ -21,4 +21,27 @@ describe('HW11 task1', () => {
         expect(response.status).toBe(201)
         expect(post.id).toBeDefined()
     })
+
+    test('Task3', async () => {
+        const changePost = {
+            title: "changed test",
+            body: "Changed test2"
+        }
+        const response = await axios.put('https://jsonplaceholder.typicode.com/posts/1', changePost)
+        const post = response.data
+        expect(response.status).toBe(200)
+        expect(post.body).toBe(changePost.body)
+        expect(post.title).toBe(changePost.title)
+    })
+
+        test('Task4', async () => {
+        const changePost = {
+            title: "changed test",
+            body: "Changed test2"
+        }
+        const response = await axios.delete('https://jsonplaceholder.typicode.com/posts/1')
+        const post = response.data
+        expect(response.status).toBe(200)
+    })
+
 })
